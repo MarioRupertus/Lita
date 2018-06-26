76ebf0ddf1cfcab0b4b858038f66e5f5da4ed8e4
User <User@TD111K-083>
2018-06-26 19:09:28 -0300

    mejorando la clase servicios

--- src/main/java/edu/mil/cet002/compraslita/Servicios.java
+++ src/main/java/edu/mil/cet002/compraslita/Servicios.java
@@ -1,33 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.mil.cet002.compraslita;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author User
- */
 public class Servicios {
+
     private List<Producto> listaDeProductos;
+    private List<Comercio> listaDeComercios;
+    private Mapa mapa;
     private LitaDB litaDB;
-    
+
     private static final Servicios servicios = new Servicios();
-    
-    public static Servicios getInstance(){
+
+    public static Servicios getInstance() {
         return servicios;
     }
-    
-    private Servicios(){
+
+    private Servicios() {
         litaDB = new LitaDB();
     }
 
-    public List<Producto> buscarProducto(String nombre){
+    public List<Producto> buscarProducto(String nombre) {
         return litaDB.buscarProductoPorNombre(nombre);
+    }
+
+    public void calificarComercio(Comercio comercio) {
         
+    }
+
+    public void calcularRecorrido(Recorrido recorrido) {
+    }
+    
+    public Carrito crearCarrito(){
+        Carrito carrito = new Carrito();
+        return carrito;
+    }
+    
+     public List<Producto> buscarProductoReal(String nombre) {
+          List<Producto> listaDeProductosBuscados = new ArrayList<>();
+//        Comercio c1 = new Comercio(9, 18);
+//        Comercio c2 = new Comercio(9, 20);
+//        Comercio c3 = new Comercio(7, 18);
+        
+        lista.add(new Producto p1);
+        lista.add(new Producto p2);
+        lista.add(new Producto p3);
+        return listaDeProductosBuscados;
     }
 }
